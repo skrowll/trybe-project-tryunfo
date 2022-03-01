05942c4af402e8caf70df675db1d27650781f988
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-03-01 17:11:23 -0300

    Requisito 6 OK

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
       cardTrunfo: false,
       // hasTrunfo: false,
       isSaveButtonDisabled: true,
+      deck: [],
     };
     this.onInputChange = this.onInputChange.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
@@ -33,7 +34,35 @@ class App extends React.Component {
     }, this.validate);
   }
 
-  onSaveButtonClick() { }
+  onSaveButtonClick() {
+    const { state } = this;
+    const newCard = {
+      cardName: state.cardName,
+      cardDescription: state.cardDescription,
+      cardAttr1: state.cardAttr1,
+      cardAttr2: state.cardAttr2,
+      cardAttr3: state.cardAttr3,
+      cardImage: state.cardImage,
+      cardRare: state.cardRare,
+      cardTrunfo: state.cardTrunfo,
+    };
+
+    this.setState((prevState) => ({
+      deck: [...prevState.deck, newCard],
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      // hasTrunfo: false,
+      isSaveButtonDisabled: true,
+    }));
+
+    console.log(state.deck);
+  }
 
   validate() {
     const {
@@ -56,8 +85,6 @@ class App extends React.Component {
     const isAttrsOnRange = (ptsAttr1 + ptsAttr2 + ptsAttr3 <= maxAttrPtsSum);
     const fields = [cardName, cardImage, cardDescription];
     const emptyFields = fields.every((field) => field !== '');
-
-    console.log(isAttrsOnRange);
 
     const isValid = (
       emptyFields
